f016e36fc6660f2b0311d83a3b2c8a8b88171e69
Daniel Frevel <daniel-frevel@web.de>
2026-01-23 13:45:05 +0100

    Lightmode darkmode switch added

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ThemeSwitcherComponent } from './shared/components/theme-switcher/theme-switcher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet />`
+  imports: [RouterOutlet, ThemeSwitcherComponent],
+  template: `
+    <div class="fixed top-4 right-4 z-50">
+      <app-theme-switcher />
+    </div>
+    <router-outlet />
+  `
 })
 export class App {}
